8335f4a21da44a6ebc36edb0382410aee9007030
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-03-11 20:02:27 -0300

    first commit

---
+++ src/test/java/com/codigo_de_sabores/prototipo/PrototipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo_de_sabores.prototipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
