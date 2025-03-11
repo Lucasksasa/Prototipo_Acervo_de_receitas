4de291727a9a55f441221e79968372d7e28f598b
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-03-11 20:20:20 -0300

    Classe Recipe implementada no pacote model

---
+++ src/main/java/com/codigo_de_sabores/prototipo/model/Recipe.java
@@ -0,0 +1,21 @@
+package com.codigo_de_sabores.prototipo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<Ingredient> ingredients;
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+}
