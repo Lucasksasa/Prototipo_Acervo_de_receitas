25d9f3208e166eb40b5eb0e36aa7145b3f6ee2a1
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-03-12 10:11:12 -0300

    Classe BookService implementada no pacote service

---
+++ src/main/java/com/codigo_de_sabores/prototipo/service/BookService.java
@@ -0,0 +1,54 @@
+package com.codigo_de_sabores.prototipo.service;
+
+import com.codigo_de_sabores.prototipo.model.Book;
+import com.codigo_de_sabores.prototipo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Cadastrar um novo livro
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Atualizar um livro existente
+    public Book updateBook(Long id, Book updatedBook) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+
+        if (optionalBook.isPresent()) {
+            Book existingBook = optionalBook.get();
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setRecipes(updatedBook.getRecipes());
+            return bookRepository.save(existingBook);
+        } else {
+            throw new RuntimeException("Livro não encontrado.");
+        }
+    }
+
+    // Remover um livro por ID
+    public void deleteBook(Long id) {
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Livro não encontrado.");
+        }
+    }
+
+    // Buscar um livro por ID
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Livro não encontrado."));
+    }
+
+    // Listar todos os livros
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
